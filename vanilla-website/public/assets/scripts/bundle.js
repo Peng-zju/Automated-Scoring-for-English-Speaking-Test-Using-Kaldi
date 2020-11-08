50858a200b96ca26eef7a38c0a772a2f5f7d1e4b
Peng Yi <yipeng.18@qq.com>
2020-11-08 06:23:03 -0800

    add audio capturing with Recorder.js and custom text box

---
+++ vanilla-website/public/assets/scripts/bundle.js
@@ -0,0 +1,30 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+(() => {
+/*!*****************************!*\
+  !*** ./src/scripts/main.js ***!
+  \*****************************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+eval("\n\n//# sourceURL=webpack://vanilla-website/./src/scripts/main.js?");
+})();
+
+(() => {
+/*!******************************!*\
+  !*** ./src/styles/main.scss ***!
+  \******************************/
+/*! namespace exports */
+/*! exports [not provided] [no usage info] */
+/*! runtime requirements:  */
+eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nModuleBuildError: Module build failed (from ./node_modules/css-loader/dist/cjs.js):\\nError: Can't resolve '../../public/assets/images/venice-italy.jpg' in '/mnt/c/Users/yipeng/OneDrive - sfu.ca/ZJU/thesis/code/product/frontend/vanilla-website/src/styles'\\n    at finishWithoutResolve (/mnt/c/Users/yipeng/OneDrive - sfu.ca/ZJU/thesis/code/product/frontend/vanilla-website/node_modules/enhanced-resolve/lib/Resolver.js:293:18)\\n    at doResolve (/mnt/c/Users/yipeng/OneDrive - sfu.ca/ZJU/thesis/code/product/frontend/vanilla-website/node_modules/enhanced-resolve/lib/Resolver.js:362:15)\\n    at hook.callAsync (/mnt/c/Users/yipeng/OneDrive - sfu.ca/ZJU/thesis/code/product/frontend/vanilla-website/node_modules/enhanced-resolve/lib/Resolver.js:410:5)\\n    at _fn1 (eval at create (/mnt/c/Users/yipeng/OneDrive - sfu.ca/ZJU/thesis/code/product/frontend/vanilla-website/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:16:1)\\n    at hook.callAsync (/mnt/c/Users/yipeng/OneDrive - sfu.ca/ZJU/thesis/code/product/frontend/vanilla-website/node_modules/enhanced-resolve/lib/Resolver.js:410:5)\\n    at _fn0 (eval at create (/mnt/c/Users/yipeng/OneDrive - sfu.ca/ZJU/thesis/code/product/frontend/vanilla-website/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:27:1)\\n    at resolver.doResolve (/mnt/c/Users/yipeng/OneDrive - sfu.ca/ZJU/thesis/code/product/frontend/vanilla-website/node_modules/enhanced-resolve/lib/DescriptionFilePlugin.js:87:43)\\n    at hook.callAsync (/mnt/c/Users/yipeng/OneDrive - sfu.ca/ZJU/thesis/code/product/frontend/vanilla-website/node_modules/enhanced-resolve/lib/Resolver.js:410:5)\\n    at _fn0 (eval at create (/mnt/c/Users/yipeng/OneDrive - sfu.ca/ZJU/thesis/code/product/frontend/vanilla-website/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at hook.callAsync (/mnt/c/Users/yipeng/OneDrive - sfu.ca/ZJU/thesis/code/product/frontend/vanilla-website/node_modules/enhanced-resolve/lib/Resolver.js:410:5)\\n    at processResult (/mnt/c/Users/yipeng/OneDrive - sfu.ca/ZJU/thesis/code/product/frontend/vanilla-website/node_modules/webpack/lib/NormalModule.js:582:19)\\n    at runLoaders (/mnt/c/Users/yipeng/OneDrive - sfu.ca/ZJU/thesis/code/product/frontend/vanilla-website/node_modules/webpack/lib/NormalModule.js:675:5)\\n    at /mnt/c/Users/yipeng/OneDrive - sfu.ca/ZJU/thesis/code/product/frontend/vanilla-website/node_modules/loader-runner/lib/LoaderRunner.js:397:11\\n    at /mnt/c/Users/yipeng/OneDrive - sfu.ca/ZJU/thesis/code/product/frontend/vanilla-website/node_modules/loader-runner/lib/LoaderRunner.js:252:18\\n    at context.callback (/mnt/c/Users/yipeng/OneDrive - sfu.ca/ZJU/thesis/code/product/frontend/vanilla-website/node_modules/loader-runner/lib/LoaderRunner.js:124:13)\\n    at Object.loader (/mnt/c/Users/yipeng/OneDrive - sfu.ca/ZJU/thesis/code/product/frontend/vanilla-website/node_modules/css-loader/dist/index.js:155:5)\\n    at process._tickCallback (internal/process/next_tick.js:68:7)\");\n\n//# sourceURL=webpack://vanilla-website/./src/styles/main.scss?");
+})();
+
+/******/ })()
+;
---
+++ vanilla-website/public/assets/scripts/utils.js
@@ -0,0 +1,32 @@
+
+var changeTextButton = document.getElementById("changeTextButton");
+var inputTextButton = document.getElementById("inputTextButton");
+var inputContainer = document.getElementById("inputContainer");
+
+
+inputTextButton.addEventListener("click", createNewElement);
+var userCustomText;
+
+function createNewElement(){
+    inputContainer.innerHTML=`
+    <form>
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1">Your custom text:</label>
+            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary" onclick="updateTextBox()">Submit</button>
+    </form>`;
+
+    changeTextButton.classList.add("hidden");
+    inputTextButton.classList.add("hidden");
+
+}
+
+
+function updateTextBox(){
+    userCustomText = document.getElementById("exampleFormControlTextarea1").value;
+    inputContainer.innerHTML=userCustomText;
+    changeTextButton.classList.remove("hidden");
+    inputTextButton.classList.remove("hidden");
+}
+
