7e354de2c73ea408bf3099e7ac7a83bf4922347e
Ubuntu <ubuntu@ip-172-31-59-141.us-west-2.compute.internal>
2020-11-13 21:13:29 +0000

    add gop_to_json.py to convert gop output to json format v2

---
+++ web-app/utils/gop_to_json.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import json
+import os
+import re
+
+def gop_to_json(wav_path):
+    wav_file=os.path.basename(wav_path)
+    wav_id=os.path.splitext(wav_file)[0].replace('-', '_')
+    web_dir = '/home/ubuntu/kaldi/egs/gop/s5/web'
+    gop_path = os.path.join(web_dir, wav_id+'/exp/gop/gop.1.txt')
+    phones_dict_path = os.path.join(web_dir, wav_id+'/exp/gop/phones-pure.txt')
+    with open(gop_path, 'r') as gop_file:
+        gop_text = gop_file.read().replace('\n', '')
+    phones_dict = pd.read_csv(phones_dict_path, header=None, delimiter=r"\s+")
+    result = []
+    for phone_gop in gop_text.split("[")[1:]:
+        phone, gop = phone_gop.split(" ")[1:3]
+        result.append([phones_dict.iloc[int(phone)][0],gop])
+    return json.dumps([{x:y} for x,y in result])
